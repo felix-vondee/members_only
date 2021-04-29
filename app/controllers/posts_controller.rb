df35de9739facce703f3a34b50dfae8026276fc7
felix_vondee <felixvondee8@gmail.com>
2021-04-29 15:31:07 +0200

    Create view for new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,28 +1,33 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
-
-  private
-
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url # halts request cycle
-    end
+  before_action only: [:new, :create]
+ 
+  def index
+    @posts = Post.all
   end
+  
+
+  # def require_login
+  #   unless logged_in?
+  #     flash[:error] = "You must be logged in to access this section"
+  #     redirect_to new_login_url # halts request cycle
+  #   end
+  # end
 
   def new
     @post = Post.new
   end
 
-  def create
+  def create 
     @post = Post.new(post_params)
+    @post.user_id = 1
     if @post.save
-        redirect_to @post 
+        redirect_to 
         else
-            render:new 
-        end
+            render:new
     end
   end
+
+  private
 
   def post_params 
     params.require(:post).permit(:title, :body, :user)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,4 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true 
   validates :body, presence: true, length: { minimum: 5 } 
-
 end
