8033d0fe47f1634d5fc36070471995a38b397db2
felix_vondee <felixvondee8@gmail.com>
2021-04-30 16:31:01 +0200

    Add dummy users to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "felix", email:'felix@gmail.com', password:'ay98776')
+User.create(name: "bishoy", email:'bishoy@gmail.com', password:'749ouh')
+
+puts 'Dummy users created'
+
+3.times do |i|
+  Post.create(title: "Post-#{i} Title", body: "This is the body of this post", user: User.last)
+end
