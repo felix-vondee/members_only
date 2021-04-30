ce4c48740f601ddc351cea0b3051d800c10addb1
felix_vondee <felixvondee8@gmail.com>
2021-04-30 19:18:22 +0200

    Use helpers to separate logic from views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+
+    def display_buttons
+      out = ''
+      if user_signed_in?
+        out << link_to('Add Post', new_post_path)
+        out << button_to('logout', destroy_user_session_path, method: :delete)
+        else
+        out << button_to('Sign in', new_user_session_path)
+      end
+      out.html_safe
+    end
 end
